1288488d273398bdb7b842c49fded6fc91b0495a
Viktor Tishchenko <9043258451vvt@gmail.com>
2021-07-30 20:21:07 +0600

    feat: gulp task runner

---
+++ src/assets/js/app.js
@@ -0,0 +1,11 @@
+//= ./components/modal.js
+/*
+ * //= ../../../node_modules/bootstrap/js/dist/util.js
+ * //= ../../../node_modules/bootstrap/js/dist/modal.js
+ * //= ../../../node_modules/slick-carousel/slick/slick.js
+ *
+ * $(function () {
+ * //= components/slick.js
+ * });
+ */
+console.log("Hello from appjs");
